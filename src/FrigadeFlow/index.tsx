86cee0ab1f09677e8e51b3a3d8db6456710b4399
Christian Mathiesen <christian@frigade.com>
2023-01-18 12:40:55 -0800

    fix(bugs): fixes a series of bugs including text wrapping issues. Adds functionality of custom variables

--- src/FrigadeFlow/index.tsx
+++ src/FrigadeFlow/index.tsx
@@ -5,22 +5,34 @@ import {useGetMyFlow} from "../api/flows";
 
 interface FrigadeFlowProps extends OnboardFlowProps, TextStyles {
   flowId: string;
+  customVariables?: object;
 }
 
 export const FrigadeFlow: FC<FrigadeFlowProps> = ({
                                                     flowId,
+                                                    customVariables,
                                                     ...props
                                                   }) => {
   const {flow, error, mutate, isLoading} = useGetMyFlow(flowId);
+
 
   if (isLoading) {
     return null;
   }
 
+  let rawData = flow.data;
+
+  if (customVariables) {
+    // Replace every ${variable} with the value of customVariables[variable] in rawData
+    for (const [key, value] of Object.entries(customVariables)) {
+      rawData = rawData.replace(new RegExp(`\\$\\{${key}\\}`, 'g'), value);
+    }
+  }
+
   return (
     <OnboardFlow
       {...props}
-      pages={JSON.parse(flow.data).data as PageData[]}
+      pages={JSON.parse(rawData).data as PageData[]}
     />
   );
 }
