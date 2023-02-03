8ea43e6198eddf500e0c44cbd42d8996f43eeb0c
christianmat <christian_mathiesen@alumni.brown.edu>
2023-02-02 17:06:44 -0800

    fix(headers): remove bad headers

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,28 +1,26 @@
-import React, {useMemo} from "react";
-import {FrigadeContext} from "../FrigadeProvider";
+import React, { useMemo } from 'react'
+import { FrigadeContext } from '../FrigadeProvider'
 
-export const API_PREFIX = 'https://api.frigade.com/v1/public/';
-
+export const API_PREFIX = 'https://api.frigade.com/v1/public/'
 
 export function useConfig() {
-  const {publicApiKey, userId} = React.useContext(FrigadeContext);
-  
+  const { publicApiKey, userId } = React.useContext(FrigadeContext)
+
   return {
     config: useMemo(
       () => ({
         headers: {
           Authorization: `Bearer ${publicApiKey}`,
-          'X-User-Id': userId ?? 'guest',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
       }),
       [publicApiKey, userId]
-    )
-  };
+    ),
+  }
 }
 
 export interface PaginatedResult<T> {
-  data: T[];
-  offset: number;
-  limit: number;
+  data: T[]
+  offset: number
+  limit: number
 }
